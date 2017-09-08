e965557857f4ca9dbcef075c758eecd91d498e8e
Ellen Biewald <ebiewald@gmail.com>
2017-09-08 18:20:49 -0400

    Putting the products on the page.
    This commit has a lot in it because it took me awhile to figure out some Angular basics.
    This sets up a list of products, sets up the Product class, and loops through the products
    printing their names, quality, and sell in values in the HTML.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Ellens App';
+  products = PRODUCTS;
 }
+
+const PRODUCTS: Product[] = [
+  { name: 'Aged Brie', quality: 40, sellIn: 6  },
+  { name: 'Sulfarus the Legendary Sword', quality: 2, sellIn: 10 },
+  { name: 'Backstage Pass', quality: 26, sellIn: 15 },
+  { name: 'Conjured Shield', quality: 18, sellIn: 1 },
+  { name: 'Regular Shield', quality: 33, sellIn: 25 }
+];
