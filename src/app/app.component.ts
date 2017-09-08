6439df3b84d2baa5248cb1987e79238fc9743280
Ellen Biewald <ebiewald@gmail.com>
2017-09-08 18:34:17 -0400

    On click of a button, decrease sellIn value.
    Add a button to the HTML to trigger update. Add a method for the button to call. Within that,
    decrement the sell in value by 1 until it hits 0.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,33 @@ export class AppComponent {
       product.quality = Math.floor(Math.random() * 50);
     }
   }
+
+  /**
+   * Update all the product sell in and quality values.
+   *
+   * We have some custom behavior based on product type.
+   * - “Aged Brie” actually increases in quality the older it gets and it never degrades.
+   * - “Sulfuras”, being a legendary item, never has to be sold and it never decreases in quality.
+   * - “Backstage Pass” increases in quality as its sell in value approaches.
+   *   Quality increases by 2 per day when there are 10 days or less,
+   *   and by 3 per day when there are 5 days or less,
+   *   but quality drops to 0 after the concert.
+   * - “Conjured” items degrade in quality twice as fast as normal items.
+  */
+  updateValues(): void {
+    for (let product of this.products) {
+      this.decreaseSellIn(product);
+    }
+  }
+
+  /**
+   * Decrease sell in value.
+   */
+  private decreaseSellIn(product): void {
+    if(product.sellIn > 0){
+      product.sellIn--;
+    }
+  }
 }
 
 const PRODUCTS: Product[] = [
